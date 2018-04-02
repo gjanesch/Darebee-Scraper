c1b2c3c50a2c7feb83f304a7e6567d31ebb8f513
gjanesch <gjanesch@gmail.com>
2018-04-01 22:08:00 -0400

    Partially completed code for specifying which workouts to choose.

---
+++ darebee_scraper_constants.py
@@ -0,0 +1,5 @@
+# Constants for the scraper
+
+DAREBEE_BASE_URL = "https://darebee.com"
+DAREBEE_FILE_NAME = "darebee.csv"
+DAREBEE_FILE_SEP = "\t"
--- darebee_scraping_functions.py
+++ darebee_scraping_functions.py
@@ -4,13 +4,13 @@ import os
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+import darebee_scraper_constants as consts
 
-DAREBEE_BASE_URL = "https://darebee.com"
-DAREBEE_FILE_NAME = "darebee.csv"
-SEP = "\t"
 
+# Shorthand for grabbing a web page
 def get_page_html(url):
     return BeautifulSoup(requests.get(url).text, "lxml")
+
 
 # Checks the main page and grabs all of the workout URLs
 def get_darebee_links(darebee_url):
@@ -37,19 +37,19 @@ def get_workout_info(workout_link):
     print(workout_link)
     workout_name = re.search("/workouts/(.*)\.html", workout_link).group(1)
     workout_name = re.sub("-", " ", workout_name).title()
-    
-    workout_page = DAREBEE_BASE_URL + workout_link
-    
+
+    workout_page = consts.DAREBEE_BASE_URL + workout_link
+
     workout_raw = requests.get(workout_page)
     workout_html = BeautifulSoup(workout_raw.text, "lxml")
-    
+
     infobox_more = workout_html.find('div', attrs={'class':'infobox'})
     focus = get_infobox(infobox_more, 'infobox-focus')
     difficulty = get_infobox(infobox_more, 'infobox-difficulty')
     works = get_infobox(infobox_more, 'infobox-works')
-    
-    pdf_url = DAREBEE_BASE_URL + "/pdf" + re.sub("\.html", ".pdf", workout_link)
-    
+
+    pdf_url = consts.DAREBEE_BASE_URL + "/pdf" + re.sub("\.html", ".pdf", workout_link)
+
     # Pages have (somewhat dramatic) descriptions of the workouts.  Could be
     # useful, though
     description = workout_html.find("div", attrs = {"class":"infomore"})
@@ -58,29 +58,32 @@ def get_workout_info(workout_link):
     else:
         description= workout_html.find("p").text
     description = re.sub("( )?\\xa0", "", description)
-    
+
     # Most (but not all) workouts have extra credit, for doing exercises more
     # intensely; grab those.
     extra_credit = workout_html.find("div", attrs = {"class":"infoec"})
     extra_credit = extra_credit.text if extra_credit is not None else ""
     extra_credit = re.sub("Extra [Cc]redit:( )?|\\xa0", "", extra_credit)
-    
+
     return (workout_name, workout_page, focus, difficulty, works, pdf_url,
             description, extra_credit)
 
+# Either updates the workout list (if the darebee file exists) or downloads all
+# of the existing workouts into a dataframe (if the file's not present).
 def create_update_workout_list():
-    workout_links = get_darebee_links(DAREBEE_BASE_URL + "/wods.html")
-    darebee_file_exists = os.path.isfile(DAREBEE_FILE_NAME)
+    workout_links = get_darebee_links(consts.DAREBEE_BASE_URL + "/wods.html")
+    darebee_file_exists = os.path.isfile(consts.DAREBEE_FILE_NAME)
     headers = ["Workout_Name","Workout_Page_URL","Focus","Difficulty",
            "Works","PDF_URL","Description","Extra_Credit"]
-    
+
     if darebee_file_exists:
         print("Darebee file found - checking for new workouts...")
-        darebee = pd.read_csv(DAREBEE_FILE_NAME, sep = "\t")
-        workout_links_full = [DAREBEE_BASE_URL + wl for wl in workout_links]
+        darebee = pd.read_csv(consts.DAREBEE_FILE_NAME, sep = consts.DAREBEE_FILE_SEP)
+        workout_links_full = [consts.DAREBEE_BASE_URL + wl for wl in workout_links]
         not_in_df = pd.Series(workout_links_full).isin(darebee["Workout_Page_URL"])
         new_workouts = [wl for wl,nid in zip(workout_links, list(~not_in_df)) if nid]
         print(str(len(new_workouts)) + " new workouts found.")
+        # If new workouts were found, download them and append them to the df
         if len(new_workouts) != 0:
             print("Gathering info...")
             new_workout_tuples = [get_workout_info(nw) for nw in new_workouts]
@@ -90,6 +93,24 @@ def create_update_workout_list():
         print("No Darebee file found - creating new file...")
         workout_tuples = [get_workout_info(wl) for wl in workout_links]
         darebee = pd.DataFrame(workout_tuples, columns = headers)
-    
-    darebee.to_csv(DAREBEE_FILE_NAME, sep = "\t", index = False)
+
+    darebee.to_csv(consts.DAREBEE_FILE_NAME, sep = consts.DAREBEE_FILE_SEP,
+                   index = False)
     print("Done.")
+
+
+# Downloads a workout PDF to the specified directory.
+def download_workout_pdf(pdf_url, file_destination):
+    response = requests.get(pdf_url)
+    with open(file_destination,'wb') as f:
+        f.write(response.content)
+
+# Sets up the workout folders; they're only divided by difficulty.
+def set_up_workout_folders():
+    if not os.path.exists("./Workout PDFs"):
+        os.mkdir("./Workout PDFs")
+
+    for x in range(5):
+        dir_name = "Difficulty " + str(x+1)
+        if not os.path.exists("./Workout PDFs/" + dir_name):
+            os.mkdir("./Workout PDFs/" + dir_name)
